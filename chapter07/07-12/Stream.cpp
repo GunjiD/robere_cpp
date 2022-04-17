ac65aabfaff3b61af18ebb97656735deca41f95f
com_gunzi <nolispnolife@gmail.com>
2022-04-17 22:37:19 +0900

    fix

---
+++ chapter07/07-12/Stream.cpp
@@ -0,0 +1,16 @@
+#include "Stream.h"
+#include <iostream>
+#include <iterator>
+
+Stream::~Stream() {}
+
+Stream::Stream() : m_n(-1) {}
+
+// 設定された値を取得
+double Stream::Get() const { return m_n; }
+
+// 値を設定
+bool Stream::Set() {
+  SetBase();
+  return m_n >= 0;
+}
